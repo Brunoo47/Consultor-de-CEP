d131dae5454378d8b619c041552153796af12a68
Brunoo47 <brunoostroskiprogramador@gamil.com>
2023-07-18 22:51:35 -0300

    gerador de cep

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import{ FiSearch} from 'react-icons/fi';
+import './style.css';
+
 
 function App() {
   return (
-    <div className="App">
-    <h1>teste react</h1>
+    <div className="container">
+      <h1 className="title">Gerador de cep</h1>
+      <div className="containerInput">
+        <input
+        type="text"
+        placeholder="Bota o cep ai"
+    />
+    <button className="ButtonSearch">
+    <FiSearch size={25} color='#000'></FiSearch>
 
-    </div>
+    </button>
+  </div>
+  <main className='main'>
+    <h2>15540386778</h2>
+
+
+    <span>Rua teste alguma</span>
+    <span>complemento</span>
+    <span>vila rosa</span>
+    <span>parana</span>
+
+
+  </main>
+
+  </div>
   );
 }
-
 export default App;
